3b474bb6bb0dd41179421a8cb853dbca0aa7051a
f <f>
2024-09-11 13:06:59 +0000

    I think the signature changed

--- OpenSimRT/Examp/src/example.cpp
+++ OpenSimRT/Examp/src/example.cpp
@@ -78,8 +78,8 @@ int main() {
 	PrescribedController* brain = new PrescribedController();
 	brain->addActuator(*biceps);
 	// Muscle excitation is 0.3 for the first 0.5 seconds, then increases to 1.
-	brain->prescribeControlForActuator("biceps",
-			new StepFunction(0.5, 3, 0.3, 1));
+	std::string actu_name = "biceps";
+	brain->prescribeControlForActuator(actu_name, StepFunction(0.5, 3, 0.3, 1));
 
 	// Add components to the model.
 	model.addBody(humerus);    model.addBody(radius);
